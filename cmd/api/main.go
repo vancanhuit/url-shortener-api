dc6244a7e7df4bf3a56899e75a095b884be73e0e
canhdinh <canhdinh@home.lab>
2023-07-15 20:46:29 +0700

    Initial setup

---
+++ cmd/api/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintln(w, `{"status": "OK"}`)
+	})
+
+	http.ListenAndServe(":9000", r)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module 192.168.1.100/homelab/url-shortener
+
+go 1.20
+
+require github.com/go-chi/chi/v5 v5.0.10
