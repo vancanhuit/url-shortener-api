3a0a7706fb15c9857cedb3afdb82e0804bcb0446
canhdinh <canhdinh@home.lab>
2023-07-22 14:54:08 +0700

    Add test cases

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,24 +1,82 @@
 package main
 
 import (
-	"fmt"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
 	"net/http"
 
+	"github.com/asaskevich/govalidator"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func main() {
+var data = make(map[string]string)
+
+func shorten(w http.ResponseWriter, r *http.Request) {
+	var req struct {
+		URL string `json:"url"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if req.URL == "" {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	if !govalidator.IsURL(req.URL) {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	hash := sha256.Sum256([]byte(req.URL))
+	alias := base64.URLEncoding.EncodeToString(hash[:])[:7]
+	data[alias] = req.URL
+
+	resp := map[string]interface{}{
+		"data": URL{
+			OriginalURL: req.URL,
+			Alias:       alias,
+		},
+	}
+
+	payload, err := json.Marshal(&resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(payload)
+}
+
+func redirect(w http.ResponseWriter, r *http.Request) {
+	alias := chi.URLParam(r, "alias")
+	url, ok := data[alias]
+	if !ok {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	http.Redirect(w, r, url, http.StatusSeeOther)
+}
+
+func router() http.Handler {
 	r := chi.NewRouter()
 
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		fmt.Fprintln(w, `{"status": "OK"}`)
-	})
+	r.Post("/api/shorten", shorten)
+	r.Get("/{alias}", redirect)
 
-	http.ListenAndServe(":9000", r)
+	return r
+}
+
+func main() {
+	http.ListenAndServe(":9000", router())
 }
---
+++ cmd/api/models.go
@@ -0,0 +1,6 @@
+package main
+
+type URL struct {
+	OriginalURL string `json:"original_url"`
+	Alias       string `json:"alias"`
+}
