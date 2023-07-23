f01443382c1b04d01f61012cc3bf580d56ce0e46
canhdinh <canhdinh@home.lab>
2023-07-24 06:34:08 +0700

    Refactor test

--- cmd/api/e2e_test.go
+++ cmd/api/e2e_test.go
@@ -126,48 +126,38 @@ func TestAPIWithValidInput(t *testing.T) {
 		} `json:"data"`
 	}
 
-	t.Run("Shorten", func(t *testing.T) {
-		reqBody := strings.NewReader(fmt.Sprintf(`{"url": "%s"}`, url))
-		statusCode, header, body := ts.do(t, http.MethodPost, "/api/shorten", reqBody)
-		require.Equal(t, http.StatusCreated, statusCode)
-		require.Equal(t, contentType, header.Get(contentTypeHeader))
-		err = json.Unmarshal(body, &envelope)
-		require.NoError(t, err)
-		alias = envelope.Data.Alias
-		require.Equal(t, url, envelope.Data.OriginalURL)
-	})
+	reqBody := strings.NewReader(fmt.Sprintf(`{"url": "%s"}`, url))
+	statusCode, header, body := ts.do(t, http.MethodPost, "/api/shorten", reqBody)
+	require.Equal(t, http.StatusCreated, statusCode)
+	require.Equal(t, contentType, header.Get(contentTypeHeader))
+	err = json.Unmarshal(body, &envelope)
+	require.NoError(t, err)
+	alias = envelope.Data.Alias
+	require.Equal(t, url, envelope.Data.OriginalURL)
 
-	t.Run("Shorten the same URL", func(t *testing.T) {
-		reqBody := strings.NewReader(fmt.Sprintf(`{"url": "%s"}`, url))
-		statusCode, header, body := ts.do(t, http.MethodPost, "/api/shorten", reqBody)
-		require.Equal(t, http.StatusCreated, statusCode)
-		require.Equal(t, contentType, header.Get(contentTypeHeader))
-		err = json.Unmarshal(body, &envelope)
-		require.NoError(t, err)
-		require.Equal(t, url, envelope.Data.OriginalURL)
-		require.Equal(t, alias, envelope.Data.Alias)
-	})
+	reqBody = strings.NewReader(fmt.Sprintf(`{"url": "%s"}`, url))
+	statusCode, header, body = ts.do(t, http.MethodPost, "/api/shorten", reqBody)
+	require.Equal(t, http.StatusCreated, statusCode)
+	require.Equal(t, contentType, header.Get(contentTypeHeader))
+	err = json.Unmarshal(body, &envelope)
+	require.NoError(t, err)
+	require.Equal(t, url, envelope.Data.OriginalURL)
+	require.Equal(t, alias, envelope.Data.Alias)
 
-	t.Run("Redirect", func(t *testing.T) {
-		statusCode, header, _ := ts.do(t, http.MethodGet, "/"+envelope.Data.Alias, nil)
-		require.Equal(t, http.StatusFound, statusCode)
-		require.Equal(t, envelope.Data.OriginalURL, header.Get("Location"))
-	})
+	statusCode, header, _ = ts.do(t, http.MethodGet, "/"+envelope.Data.Alias, nil)
+	require.Equal(t, http.StatusFound, statusCode)
+	require.Equal(t, envelope.Data.OriginalURL, header.Get("Location"))
 
-	t.Run("Delete", func(t *testing.T) {
-		statusCode, _, _ := ts.do(t, http.MethodDelete, "/"+envelope.Data.Alias, nil)
-		require.Equal(t, http.StatusNoContent, statusCode)
-	})
+	statusCode, _, _ = ts.do(t, http.MethodDelete, "/"+envelope.Data.Alias, nil)
+	require.Equal(t, http.StatusNoContent, statusCode)
 
-	t.Run("Not found URL", func(t *testing.T) {
-		statusCode, header, _ := ts.do(t, http.MethodGet, "/"+envelope.Data.Alias, nil)
-		require.Equal(t, http.StatusNotFound, statusCode)
-		require.Equal(t, contentType, header.Get(contentTypeHeader))
+	statusCode, header, _ = ts.do(t, http.MethodGet, "/"+envelope.Data.Alias, nil)
+	require.Equal(t, http.StatusNotFound, statusCode)
+	require.Equal(t, contentType, header.Get(contentTypeHeader))
 
-		statusCode, header, _ = ts.do(t, http.MethodDelete, "/"+envelope.Data.Alias, nil)
-		require.Equal(t, http.StatusNotFound, statusCode)
-		require.Equal(t, contentType, header.Get(contentTypeHeader))
-	})
+	statusCode, header, _ = ts.do(t, http.MethodDelete, "/"+envelope.Data.Alias, nil)
+	require.Equal(t, http.StatusNotFound, statusCode)
+	require.Equal(t, contentType, header.Get(contentTypeHeader))
 }
 
 func TestAPIWithInvalidInput(t *testing.T) {
